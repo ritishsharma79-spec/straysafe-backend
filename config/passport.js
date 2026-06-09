085a073c37e37a9baaa384596eb576164e1c22a1
ritish sharma <ritishsharma79@gmail.com>
2026-06-09 21:55:40 +0530

    use env variable for callback URL

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,7 @@ passport.use(
     {
       clientID:     process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:  "http://localhost:5000/auth/google/callback",
+      callbackURL:   `${process.env.BACKEND_URL}/auth/google/callback`,
     },
     // This runs after Google confirms the user is real
     // profile contains their Google account info — name, email, photo
