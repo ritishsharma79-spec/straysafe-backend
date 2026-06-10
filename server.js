3318b7ad4c5cc0f399f4d8bf763fff7e173be8bd
ritish sharma <ritishsharma79@gmail.com>
2026-06-10 12:20:15 +0530

    trust proxy for Railway deployment

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import authRoutes from "./routes/auth.js";
 dotenv.config();
 
 const app = express();  //creating a server obj similar to app=flask(__name__)
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
