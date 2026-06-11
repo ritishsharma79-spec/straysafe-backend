258205a90073549aa031f2c0ad49be6744645d20
ritish sharma <ritishsharma79@gmail.com>
2026-06-12 00:38:17 +0530

    newtimeout  email

--- config/email.js
+++ config/email.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+   connectionTimeout: 10000, // 10 seconds
+   greetingTimeout: 10000,
+   socketTimeout: 15000,
 });
 
 export const sendAnimalListedEmail = async ({ toEmail, toName, animalName }) => {
